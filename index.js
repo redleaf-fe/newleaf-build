92fdbcc415f93301e5ad4f3fdcd2a00d70c712c7
vzhufeng <vzhufeng@163.com>
2021-07-12 10:40:17 +0800

    feat: update index.js

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const Logger = require('koa-logger');
-const BodyParser = require('koa-body');
+const Koa = require("koa");
+const Router = require("koa-router");
+const Logger = require("koa-logger");
+const BodyParser = require("koa-body");
 
-const config = require('./env.json');
+const config = require("./env.json");
 
 async function main() {
-
   const app = new Koa();
   const router = new Router();
 
   // 跨域配置
   app.use(async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', 'content-type');
+    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.set("Access-Control-Allow-Headers", "content-type");
     await next();
   });
 
-  if(config.dev){
+  if (config.dev) {
     app.use(Logger());
   }
-  
+
   app.use(BodyParser());
 
-  require('./routes')(router);
+  require("./routes")(router);
 
   app.use(router.routes());
   app.use(router.allowedMethods());

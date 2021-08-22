5c62afdd678e3ef5c71ae28e64843a15f71af974
vzhufeng <vzhufeng@163.com>
2021-08-22 16:49:07 +0800

    feat: build ip addr read from env

--- index.js
+++ index.js
@@ -11,29 +11,6 @@ const path = require("path");
 const redisKey = require("./redisKey");
 const config = require("./env.json");
 
-function getIPAddress() {
-  const interfaces = os.networkInterfaces();
-  let address;
-  Object.keys(interfaces).some((v) => {
-    const iface = interfaces[v];
-    for (let i = 0; i < iface.length; i++) {
-      const alias = iface[i];
-      if (
-        alias.family === "IPv4" &&
-        alias.address !== "127.0.0.1" &&
-        !alias.internal
-      ) {
-        address = alias.address;
-        return true;
-      }
-    }
-    return false;
-  });
-  return address;
-}
-
-const IPAddr = getIPAddress();
-
 async function main() {
   const app = new Koa();
   const router = new Router();
@@ -93,7 +70,7 @@ async function main() {
           appId,
           commit,
           appName,
-          addr: `http://${IPAddr}:${config.serverPort}`,
+          addr: `http://${buildAddress}:${config.serverPort}`,
         },
       });
     }
